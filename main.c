6f4ad513503712c1fca6b005f700ffdeb2593583
leejeanah <ijina590@naver.com>
2022-10-11 17:44:57 +0900

    05

--- main.c
+++ main.c
@@ -2,15 +2,39 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-int square(int a)
+int main(void)
 {
-	return a*a;
+	int n, r, i, res;
+	
+	printf("두 정수를 입력하시오: ");
+	scanf("%d %d", &n, &r);
+	
+	res = combination(n, r);
+	printf("c(%d, %d) = %d", n, r, res);
+	
+	return 0; 
 }
 
-int main(int argc, char *argv[]){
-	int a = 2;
+int combination(int n,int r)
+{
+ int a, b, c, result;
+ 
+ a = factorial(n);
+ b = factorial(n-r);
+ c = factorial(r);
+
+ result = a/(b*c);
+ 
+  return result;
+ }	
+
+int factorial(int x)
+{
+	int res = 1;
+	int i;
+	for(i=1; i<x+1; i++){
+		res = res*i;
 	
-	a = square(a);
-	
-	printf("a=%i\n", a);
+}
+	return res;
 }
